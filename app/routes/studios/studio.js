50afe9469b8cf6087a0be74e816207122e50d30a
WerkerM <michael.handwerker@gmail.com>
2016-06-16 14:57:54 -0400

    fixed the data passing between studio index and show pages

--- app/routes/studios/studio.js
+++ app/routes/studios/studio.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    return this.store.findAll('studio');
+  model: function(params){
+    return this.store.peekRecord('studio', params.studio_id);
   }
 });
