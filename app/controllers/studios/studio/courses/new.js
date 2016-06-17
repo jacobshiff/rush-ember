c48c7552750830c4d1b3958f5b44fdb55cbd0e52
WerkerM <michael.handwerker@gmail.com>
2016-06-17 14:51:27 -0400

    new course creation working

--- app/controllers/studios/studio/courses/new.js
+++ app/controllers/studios/studio/courses/new.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     submit() {
-      this.get('model').save()
-      // .then((course) => {
-      //   this.transitionToRoute('studios.studio.courses.course', course.studio, course)
-      // })
+      this.get('model').save().then((course) => {
+        debugger;
+        this.transitionToRoute('studios.studio.courses.course', course.studio, course);
+      });
     }
   }
 });
--- app/routes/studios/studio/courses/course.js
+++ app/routes/studios/studio/courses/course.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    // return this.store.findRecord('studio', params.studio_id);
     return this.store.peekRecord('course', params.course_id);
   }
 });
