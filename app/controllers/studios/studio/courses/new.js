783df0b20267ed46fc650387a66b4197f22f6efe
jacobshiff <jacobshiff@users.noreply.github.com>
2016-06-17 15:23:11 -0400

    Update new.js

--- app/controllers/studios/studio/courses/new.js
+++ app/controllers/studios/studio/courses/new.js
@@ -4,7 +4,6 @@ export default Ember.Controller.extend({
   actions: {
     submit() {
       this.get('model').save().then((course) => {
-        debugger;
         this.transitionToRoute('studios.studio.courses.course', course.studio, course);
       });
     }
