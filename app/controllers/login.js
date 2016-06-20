47863b2ae2d652510dc033ae61c7de4cff213f3d
WerkerM <michael.handwerker@gmail.com>
2016-06-20 17:26:21 -0400

    refactored auth

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,14 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({  
+export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
   actions: {
     authenticate: function() {
       var credentials = this.getProperties
-        ('identification', 'password'),
+        ('email', 'password'),
         authenticator = 'authenticator:jwt';
-      this.get('session').authenticate(authenticator, 
+      this.get('session').authenticate(authenticator,
         credentials).catch((reason)=>{
         this.set('errorMessage', reason.error || reason);
       });
