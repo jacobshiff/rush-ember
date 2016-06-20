cf5f23e4ebef9bfa4089835fad0bb14109bc1231
jacobshiff <jacobshiff@gmail.com>
2016-06-20 18:21:58 -0400

    almost working but not quite

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,25 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
+const { service } = Ember.inject;
+
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  sessionAccount: service('session-account'),
+
+  beforeModel() {
+    return this._loadCurrentUser();
+  },
+
+  sessionAuthenticated() {
+    this._loadCurrentUser().then(()=>{
+      this.transitionTo('/courses');
+    }).catch(() => this.get('session').invalidate());
+  },
+
+  _loadCurrentUser() {
+    return this.get('sessionAccount').loadCurrentUser();
+  },
+
   actions: {
     invalidateSession: function() {
         this.get('session').invalidate();
