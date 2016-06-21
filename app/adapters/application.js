ea88764430110548d1550cc110c748ab3580af26
WerkerM <michael.handwerker@gmail.com>
2016-06-21 11:08:25 -0400

    modified environment config

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,14 +1,9 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';  
-
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+import config from '../config/environment'
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   namespace: 'api/v1',
-  host: 'http://localhost:3000',
+  host: config.host,
   authorizer: 'authorizer:custom'
 });
-
-// export default JSONAPIAdapter.extend({
-//   namespace: 'api/v1',
-//   host: 'http://localhost:3000'
-// });
--- config/environment.js
+++ config/environment.js
@@ -19,7 +19,7 @@ module.exports = function(environment) {
     }
   };
 
-  ENV['ember-simple-auth'] = {  
+  ENV['ember-simple-auth'] = {
     authorizer: 'authorizer:custom',
     routeAfterAuthentication: '/'
   };
@@ -46,7 +46,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.host = 'http://guarded-ridge-9945.herokuapp.com';
   }
 
   return ENV;
